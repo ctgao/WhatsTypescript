1c733b743a7c9cbab2ee4ce51ec10c690b912bdb
cgao_ksm <137011200+ctgao@users.noreply.github.com>
2023-11-08 15:24:39 -0500

    The button works, and passing props in works as well

--- src/Definition.tsx
+++ src/Definition.tsx
@@ -1,8 +1,10 @@
 const Definition = ({ word }: { word: string }) => {
+  const displayWord = word ? word : "definition";
+
   return (
     <main className="container">
       <div className="bg-light p-5 rounded">
-        <h1>{word ? word : "Definition"}</h1>
+        <h1>{displayWord.charAt(0).toUpperCase() + displayWord.substring(1).toLowerCase()}</h1>
         <p className="lead">
           Use this part of the page to present your results from the API call.
         </p>
@@ -10,6 +12,7 @@ const Definition = ({ word }: { word: string }) => {
           <li>This is a list. Your word could go here.</li>
           <li>It appears completely un-styled.</li>
           <li>Maybe your multiple definitions of the word are here?</li>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
           <li>Structurally, it's still a list.</li>
           <li>However, this style only applies to immediate child elements.</li>
           <li>
